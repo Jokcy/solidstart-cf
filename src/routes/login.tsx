00367121fc2bab6521ec6693649b6deac79b3bf3
jokcy.lou <jokcy.lou@ringcentral.com>
2024-01-30 16:44:43 +0800

    add log

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -3,17 +3,20 @@ import * as Card from "~/components/Card";
 import { css } from "~/styled-system/css";
 // import {} from "@solidjs/start";
 import { getRequestEvent } from "solid-js/web";
+import { createResource } from "solid-js";
 
 function hasDB() {
     "use server";
 
     const requestEvent = getRequestEvent();
 
+    console.log(requestEvent?.context);
+
     return Object.keys(requestEvent?.context?.cloudflare?.DB || {}).join(",");
 }
 
 export default function Login() {
-    const inDB = hasDB();
+    const [inDB] = createResource(hasDB);
 
     return (
         <div
@@ -26,7 +29,7 @@ export default function Login() {
         >
             <Card.Root>
                 <Card.Header>
-                    <Card.Title>Sign In {inDB}</Card.Title>
+                    <Card.Title>Sign In {inDB()}</Card.Title>
                 </Card.Header>
                 <Card.Body>
                     <Button>Login Via Github</Button>
