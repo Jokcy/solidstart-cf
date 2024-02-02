54585d1a586478d060404add5594d3946642f8cf
Jokcy <jokcylou@hotmail.com>
2024-02-02 18:52:42 +0800

    remove api

--- src/routes/api/test.ts
+++
@@ -1,8 +0,0 @@
-import { type APIEvent } from "@solidjs/start/server";
-import { getDB } from "~/server/database/db";
-
-export function GET(event: APIEvent) {
-    const db = getDB();
-
-    return new Response(event.context.cloudflare);
-}
