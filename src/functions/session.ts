d1e94c30361cc1c046197df98f493d0a8e1fd925
Jokcy <jokcylou@hotmail.com>
2024-02-02 21:53:11 +0800

    hard code secret

--- src/functions/session.ts
+++ src/functions/session.ts
@@ -12,9 +12,9 @@ export type Session = {
 
 const [session, setSession] = createSignal<Session | null>();
 
-const SECRET = process.env.JWT_SECRET!;
-
-const secret = new TextEncoder().encode(SECRET);
+const secret = new TextEncoder().encode(
+    "cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2",
+);
 
 export const signJWT = async (payload: any) => {
     "use server";
